f75f2704ecc7415f79a7b869e0a7176c54e63dfa
nazik360 <nazarrusin19@gmail.com>
2026-05-14 13:24:45 +0300

    fix keymapping

--- src/main/java/com/cynosphere/perspectivemodredux/PerspectiveModRedux.java
+++ src/main/java/com/cynosphere/perspectivemodredux/PerspectiveModRedux.java
@@ -15,21 +15,14 @@ public class PerspectiveModRedux implements ClientModInitializer {
     public static float savedPitch = 0f;
     public static float perspYaw = 0f;
     public static float perspPitch = 0f;
-
     private static KeyMapping perspectiveKey;
     private static boolean prevKeyDown = false;
 
-    public static final KeyMapping.Category CATEGORY =
-        KeyMapping.Category.create("key.categories.perspectivemodredux", 100);
-
     @Override
     public void onInitializeClient() {
-        perspectiveKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-            "key.perspectivemodredux.toggle",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_F4,
-            CATEGORY
-        ));
+        perspectiveKey = KeyBindingHelper.registerKeyBinding(
+            new KeyMapping("key.perspectivemodredux.toggle", GLFW.GLFW_KEY_F4, "key.categories.misc")
+        );
         ClientTickEvents.END_CLIENT_TICK.register(this::onClientTick);
     }
 
